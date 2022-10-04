a110a092fd4d755590d4fbc8b542f1e8644f8435
Nur Muhammad Himawan <muhammad.himawan73@gmail.com>
2022-10-04 13:22:18 +0700

    fix responsive for mobile

--- script.js
+++ script.js
@@ -21,31 +21,31 @@ const valuesCards = [{
     image: 'assets/img/2.png',
     title: 'Justin Geber',
     instagram: 'https://www.instagram.com/justinbieber/',
-    youtube: 'https://www.youtube.com/c/ArianaGrande/',
+    youtube: 'https://www.youtube.com/c/justinbieber/',
 },
 {
     image: 'assets/img/3.png',
-    title: 'Ariana Grandong',
-    instagram: 'https://www.instagram.com/arianagrande/',
-    youtube: 'https://www.youtube.com/c/ArianaGrande/'
+    title: 'Selena Gowes',
+    instagram: 'https://www.instagram.com/selenagomez/',
+    youtube: 'https://www.youtube.com/c/selenagomez/'
 },
 {
     image: 'assets/img/4.png',
-    title: 'Ariana Grandong',
-    instagram: 'https://www.instagram.com/arianagrande/',
-    youtube: 'https://www.youtube.com/c/ArianaGrande/'
+    title: 'Hailee Stempel',
+    instagram: 'https://www.instagram.com/haileesteinfeld/',
+    youtube: 'https://www.youtube.com/c/haileesteinfeld/'
 },
 {
     image: 'assets/img/5.png',
-    title: 'Ariana Grandong',
-    instagram: 'https://www.instagram.com/arianagrande/',
-    youtube: 'https://www.youtube.com/c/ArianaGrande/'
+    title: 'Billie Arisan',
+    instagram: 'https://www.instagram.com/billieeilish/',
+    youtube: 'https://www.youtube.com/c/BillieEilish/'
 }
 ]
 
 function returnCards(valuesCards) {
-    return valuesCards.map(valuesCard => `"<div class='card'>
-        <img src="${valuesCard.image}" class="user" alt="" />
+    return valuesCards.map(valuesCard => `<div class='card'>
+        <img class="round" src="${valuesCard.image}" alt="${valuesCard.title}" />
         <h3>${valuesCard.title}</h3>
         <div class="info-artist">
             <ul>
